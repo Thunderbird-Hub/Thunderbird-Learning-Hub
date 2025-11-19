a4e2265094e526afba2f087bbeae2ba16231cfad
Kyle Walker <134336392+kylew2001@users.noreply.github.com>
2025-11-20 10:07:50 +1300

    Update manage_quizzes.php

--- admin/manage_quizzes.php
+++ admin/manage_quizzes.php
@@ -744,7 +744,7 @@ include __DIR__ . '/../includes/header.php';
                             <option value="12">Every 12 months</option>
                             <option value="24">Every 24 months</option>
                         </select>
-                        <small class="form-text">Select how often users must retake this quiz after passing. This is similar to the McDonald's training system where quizzes need to be retaken periodically.</small>
+                        
                     </div>
 
                     <button type="submit" class="btn btn-primary">Create Quiz</button>
